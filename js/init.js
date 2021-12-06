798310a08c0213b33839e468a8551a7824483626
longtanle <tanlong.ce@gmail.com>
2021-12-07 00:23:44 +0700

    Update personal website

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+			strings: ["at the University of Sydney"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
